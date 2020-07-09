b6f1292ad1729bc4874e82a476f1acedc545f8b7
Gregory <gregory09ruiz@gmail.com>
2020-07-08 23:18:21 -0400

    Href_post

--- resources/views/web/posts.blade.php
+++ resources/views/web/posts.blade.php
@@ -3,6 +3,21 @@
     <div class="container">
         <div class="col-md-8 col-md-offset-2">
             <h1>Lista de Articulos</h1>
+            @foreach ($posts as $post)
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        {{ $post->name }}
+                    </div>
+                    <div class="panel-body">
+                        @if($post->file)
+                            <img src="{{ $post->file }}" alt="Imagen del Post" class="img-responsive">
+                        @endif
+                        {{ $post->excerpt }}
+                        <a href="{{ route('post', $post->slug) }}" class="pull-right">Leer Más</a>
+                    </div>
+                </div>
+            @endforeach
+            {{$posts->render()}}
         </div>
     </div>
 @endsection
