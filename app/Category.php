c765a280677cee247a8b5118d207f0d40ad526b5
Gregory <gregory09ruiz@gmail.com>
2020-07-08 22:30:03 -0400

    Category_Pertenceciente

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable =[
+        'name', 'slug', 'body'
+    ];
+
+//Una Categoria puede pertenecer a muchos Post
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -11,16 +11,16 @@ class Post extends Model
         'user_id', 'category_id', 'name', 'slug', 'excerpt', 'body', 'status', 'file'
     ];
 
-
+//Pertenece a un usuario
     public function user(){
         return $this->belongsTo(User::tag);
     }
-
+//Pertenece a una Categoria
     public function category(){
         return $this->belongsTo(Category::tag);
     }
 
-
+//Pero puede pertenecer a muchas pero muchas etiquetas
     public function tags(){
         return $this->belongsToMany(Tag::class);
     }
