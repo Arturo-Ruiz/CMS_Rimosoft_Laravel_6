8a447490dfa983fb7366c58be596593692c11643
Gregory <gregory09ruiz@gmail.com>
2020-07-08 22:25:01 -0400

    Pertence a una categoria

--- app/Post.php
+++ app/Post.php
@@ -6,12 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
+    
+
+
     public function user(){
         return $this->belongsTo(User::tag);
     }
 
-    public function user(){
-        return $this->belongsTo(User::tag);
+    public function category(){
+        return $this->belongsTo(Category::tag);
     }
 
 
