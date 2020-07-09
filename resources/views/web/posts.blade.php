9f349363944d0a17f98d94564ae175302b82011b
Gregory <gregory09ruiz@gmail.com>
2020-07-08 23:07:57 -0400

    Post-blade.content

---
+++ resources/views/web/posts.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="col-md-8 col-md-offset-2">
+            <h1>Lista de Articulos</h1>
+        </div>
+    </div>
+@endsection
