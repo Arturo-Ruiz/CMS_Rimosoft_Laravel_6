5df02c5018ea7d417368481bcd50c1074122455a
Gregory <gregory09ruiz@gmail.com>
2020-07-08 23:10:31 -0400

    Where_post

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PageController extends Controller
 {
     public function blog(){
-        return 'blog';
+        $posts = Post::orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(3);
+        return view('web.posts', compact('posts'));
     }
 }
