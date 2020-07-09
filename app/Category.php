fb453a61967e48ca575e52f7ffce5c31b7639ce4
Gregory <gregory09ruiz@gmail.com>
2020-07-08 22:30:41 -0400

    hasMany

--- app/Category.php
+++ app/Category.php
@@ -12,6 +12,6 @@ class Category extends Model
 
 //Una Categoria puede pertenecer a muchos Post
     public function posts(){
-        return $this->belongsToMany(Post::class);
+        return $this->hasMany(Post::class);
     }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable =[
+        'name', 'slug', 
+    ];
+
+//Una Categoria puede pertenecer a muchos Post
+    public function posts(){
+        return $this->belongsToMany(Post::class);
+    }
 }
