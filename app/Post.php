f381b8610a14f91e7de1febc8f1e4867eca013d4
Gregory <gregory09ruiz@gmail.com>
2020-07-08 22:24:19 -0400

    Pertenece a

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    public function user(){
+        return $this->belongsTo(User::tag);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::tag);
+    }
+
+
     public function tags(){
         return $this->belongsToMany(Tag::class);
     }
