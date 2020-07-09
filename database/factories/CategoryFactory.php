b78a8c58a1b36ad88746eda2fd1b843afbe311ce
Gregory <gregory09ruiz@gmail.com>
2020-07-08 21:44:38 -0400

    Category_factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,8 +5,11 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Category::class, function (Faker $faker) {
+    $title = $faker->sentence(4);
     return [
-        //
+        'name' => $title,
+        'slug' => str_slug($title),
+        'body' => $faker->text(500),
     ];
 });
