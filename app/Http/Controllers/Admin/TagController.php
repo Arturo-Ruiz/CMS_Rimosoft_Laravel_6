706f46a1278cb85b9462cb9eacdf5758163292e5
Gregory <gregory09ruiz@gmail.com>
2020-07-09 22:01:36 -0400

    tag_validation

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -42,7 +42,7 @@ class TagController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(TagStoreRequest $request)
     {
         $tag = Tag::create($request->all());
         return redirect()->route('tags.edit', $tag->id)->with('info', 'Etiqueta Creada Con Exito');
@@ -79,7 +79,7 @@ class TagController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(TagUpdateRequest $request, $id)
     {
         $tag= Tag::find($id);
         $tag->fill($request->all())->save();
