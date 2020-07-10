c40babde3584b521b6d569c2b8e1bf623f64af0f
Gregory <gregory09ruiz@gmail.com>
2020-07-09 22:20:52 -0400

    render_tags

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -9,35 +9,35 @@
                         Lista de Etiquetas
                         <a class="btn btn-sm btn-primary pull-right" href="{{ route('tags.create') }}">Crear</a>
                     </div>
-
-                </div>
-                <div class="panel-body">
-                    <table class="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                                <th width='10px'>ID</th>
-                                <th>Nombre</th>
-                                <th colspan="3">&nbsp;</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($tags as $tag)
+                    <div class="panel-body">
+                        <table class="table table-striped table-hover">
+                            <thead>
                                 <tr>
-                                    <td>{{$tag->id}}</td>
-                                    <td>{{$tag->name}}</td>
-                                    <td width='10px'>
-                                        <a href="{{ route('tags.show', $tag->id) }}" class="btn btn-sm btn-info">Ver</a>
-                                    </td>
-                                    <td width='10px'>
-                                        <a href="{{ route('tags.edit', $tag->id) }}" class="btn btn-sm btn-warning">Editar</a>
-                                    </td>
-                                    <td width='10px'>
-
-                                    </td>
+                                    <th width='10px'>ID</th>
+                                    <th>Nombre</th>
+                                    <th colspan="3">&nbsp;</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($tags as $tag)
+                                    <tr>
+                                        <td>{{$tag->id}}</td>
+                                        <td>{{$tag->name}}</td>
+                                        <td width='10px'>
+                                            <a href="{{ route('tags.show', $tag->id) }}" class="btn btn-sm btn-info">Ver</a>
+                                        </td>
+                                        <td width='10px'>
+                                            <a href="{{ route('tags.edit', $tag->id) }}" class="btn btn-sm btn-warning">Editar</a>
+                                        </td>
+                                        <td width='10px'>
+                                            Eliminar
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{$tags->render()}}
+                    </div>
                 </div>
             </div>
         </div>
