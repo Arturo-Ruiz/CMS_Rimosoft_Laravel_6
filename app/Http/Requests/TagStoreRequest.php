ec309a8f8104d2d8098f6c1fc49c5c584f17b15d
Gregory <gregory09ruiz@gmail.com>
2020-07-09 22:03:13 -0400

    tag_store_request_ready

--- app/Http/Requests/TagStoreRequest.php
+++ app/Http/Requests/TagStoreRequest.php
@@ -13,7 +13,7 @@ class TagStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class TagStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'=>'required',
+            'slug'=>'required|unique:tags,slug',
         ];
     }
 }
