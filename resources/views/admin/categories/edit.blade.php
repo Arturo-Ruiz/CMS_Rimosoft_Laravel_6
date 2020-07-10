2242300cf6e39a6786bdbf67ff96bcec1b4cb732
Gregory <gregory09ruiz@gmail.com>
2020-07-10 16:45:07 -0400

    Edit_category

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -7,11 +7,11 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-headig">
-                        Editar Etiqueta
+                        Editar Categoria
                     </div>
                     <div class="panel-body">
-                        {!! Form::model($tag, ['route'=>['tags.update',$tag->id], 'method'=> 'PUT']) !!}
-                            @include('admin.tags.partials.form')
+                        {!! Form::model($category, ['route'=>['categories.update',$category->id], 'method'=> 'PUT']) !!}
+                            @include('admin.categories.partials.form')
                         {!! Form::close() !!}
                     </div>
                 </div>
